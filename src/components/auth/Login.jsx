58f0be00a521f8987b43cadb9d69b3910a1770d3
salmaasal <salameri482@gmail.com>
2023-02-28 10:04:52 +0400

    updated

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,9 +2,66 @@ import React, { Component } from 'react'
 import {Link } from 'react-router-dom'
 
 export default class Login extends Component {
-  render() {
-    return (
-      <div>Login</div>
-    )
+  constructor() {
+    super();
+    // to give a call to the base class constructor
+    this.state = {
+      email: "",
+      password: "",
+    };
+    // state : its an object from the base class i.e. Component class to hold the data for our component.
   }
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value});
+    // we need to collect the data for every changed field
+    // we need to update the state accordingly.
+   // console.log(e.target.value);
+    //e : event
+    // target : location where event happened
+    // value : whatever the input that we provided it will give us that data.
+    //console.log(e.target.name); //e : event
+    // target : location where event happened
+    // name : name field value of the input tag.
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+render() {
+  const { email, password} = this.state;
+    return (
+      <> 
+      <section class="container">
+        <br />
+      <h1 class="large text-primary">Sign In</h1>
+      <p class="lead"><i class="fas fa-user"></i> Sign into Your Account</p>
+      <form class="form" onSubmit={this.onSubmit}>
+        <div class="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={this.onChange}
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={this.onChange}
+          />
+        </div>
+        <input type="submit" class="btn btn-primary" value="Login" />
+      </form>
+      <p class="my-1">
+        Don't have an account? <a href="register.html">Sign Up</a>
+      </p>
+    </section>
+    </>
+    )
 }
+};
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,41 +1,102 @@
-import React from 'react'
-import {Link } from 'react-router-dom'
+import React, { Component } from "react";
+import api from "../../utils/api";
 
-function Register() {
-  return (
-   <>
-   <section class="container">
-      <h1 class="x-large text-primary">Sign Up for a DevConnector</h1>
-      <p class="lead"><i class="fa fa-user-circle-o"></i>Create your Account</p>
-      <form class="form">
-        <div class="form-group">
-          <input type="text" placeholder="Enter the Name" name="name" />
-        </div>
-        <div class="form-group">
-          <input type="email" placeholder="enter your email" name="email" />
-          <small>This site uses Gravatar so if you want a profile image, use a
-            Gravatar email </small>
-        </div>
-        <div class="form-group">
-          <input type="password" placeholder="Password" name="password" />
-        </div>
-        <div class="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-          />
-        </div>
-        <div class="form-group">
-          <input type="date" name="dob" placeholder="Enther DOB" />
-        </div>
-        <div class="form-group">
-          <input type="submit" class="btn btn-primary" value="Register" />
-        </div>
-      </form>
-    </section>
-   </>
-  )
+
+export default class Register extends Component {
+  constructor() {
+    super();
+    // to give a call to the base class constructor
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+    // state : its an object from the base class i.e. Component class to hold the data for our component.
+  }
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value});
+    // we need to collect the data for every changed field
+    // we need to update the state accordingly.
+   // console.log(e.target.value);
+    //e : event
+    // target : location where event happened
+    // value : whatever the input that we provided it will give us that data.
+    //console.log(e.target.name); //e : event
+    // target : location where event happened
+    // name : name field value of the input tag.
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    api.post('/users', this.state);
+    //it will accept the end point(URL0)
+    // the data 
+    //the headers 
+  };
+
+  render() {
+    // destructuring the state object
+    const { name, email, password, confirmPassword } = this.state;
+    return (
+      <>
+        {" "}
+        <section class="container">
+          <h1 class="large text-primary">Sign Up</h1>
+          <p class="lead">
+            <i class="fas fa-user"></i> Create Your Account
+          </p>
+          <form class="form" onSubmit={this.onSubmit}>
+            <div class="form-group">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                required
+                value={name}
+                onChange={this.onChange}
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="email"
+                placeholder="Email Address"
+                name="email"
+                value={email}
+                onChange={this.onChange}
+              />
+              <small class="form-text">
+                This site uses Gravatar so if you want a profile image, use a
+                Gravatar email
+              </small>
+            </div>
+            <div class="form-group">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                minLength="6"
+                value={password}
+                onChange={this.onChange}
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                name="confirmPassword"
+                minLength="6"
+                value={confirmPassword}
+                onChange={this.onChange}
+              />
+            </div>
+            <input type="submit" class="btn btn-primary" value="Register" />
+          </form>
+          <p class="my-1">
+            Already have an account? <a href="login.html">Sign In</a>
+          </p>
+        </section>
+      </>
+    );
+  }
 };
-
-export default Register
